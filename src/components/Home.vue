2ac1614626e2036cb40d48a7352264508b9554f5
leejua1 <1479q@naver.com>
2020-05-18 09:45:07 +0900

    0518

---
+++ src/components/Home.vue
@@ -0,0 +1,56 @@
+<template>
+    <div id="app">
+        <Layout>
+            <template #header = "self">
+                <router-link to="/home"><h1 id ="title">{{self.title}}</h1></router-link>
+                <router-link to="/join"><span id = "join">{{self.join}}</span></router-link>
+                <router-link to = "/login"><span id = "login">{{self.login}}</span></router-link>
+            </template>
+            <template #sidebar>
+
+                <ul class = "menu">
+                    <li v-for="i of sidebars" v-bind:key="i.menu">
+                        {{i.menu}}
+
+                    </li></ul>
+            </template>
+            <template #content>
+                <router-view/>
+
+            </template>
+            <template #footer="self">
+                <h3 id = "footer"> {{self.footer}}</h3>
+            </template>
+        </Layout>
+    </div>
+</template>
+
+<script>
+    import Layout from "./common/Layout.vue"
+    export default {
+        components : {Layout},
+        data (){
+            return {
+                sidebars: [
+                        {menu: '쓰기'},
+                        {menu: '목록'},
+                        {menu: '검색'},
+                        {menu: '수정'},
+                        {menu: '삭제'}
+                ]
+            }
+        }
+    }
+</script>
+<style scoped>
+    ul.menu{
+        position: relative;
+        padding: 5px;
+        list-style: none;
+        font-style: italic;
+    }
+    #title{width: 300px;margin: 0 auto}
+    #login{margin-right: 50px; float: right}
+    #join{margin-right: 50px; float: right}
+    #footer{width: 300px; margin: 0 auto}
+</style>
